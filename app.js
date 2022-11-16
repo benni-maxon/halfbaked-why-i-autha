83b1e83e079292c7f5aca0bca7b9ad23f62ff837
benni maxon <benni.maxon@gmail.com>
2022-11-16 12:16:47 -0800

    defined dom elements in app.js and built signUpForm event listener

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+import { redirectIfLoggedIn, signUpUser } from './fetch-utils.js';
+
 const signInForm = document.getElementById('sign-in');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
 
 const signUpForm = document.getElementById('sign-up');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
 
 // Wire up sign in and sign up forms to supabase
+redirectIfLoggedIn();
+
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const user = await signUpUser(signUpEmail.value, signUpPassword.value);
+
+    if (user) {
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
