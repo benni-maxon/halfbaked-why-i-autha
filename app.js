6cacfe2648f674adb9a26e1076db398dedbf0106
benni maxon <benni.maxon@gmail.com>
2022-11-16 13:00:37 -0800

    updated signUpForm and signInForm to use FormData

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 import { redirectIfLoggedIn, signInUser, signUpUser } from './fetch-utils.js';
 
 const signInForm = document.getElementById('sign-in');
-const signInEmail = document.getElementById('sign-in-email');
-const signInPassword = document.getElementById('sign-in-password');
-
 const signUpForm = document.getElementById('sign-up');
-const signUpEmail = document.getElementById('sign-up-email');
-const signUpPassword = document.getElementById('sign-up-password');
 
 redirectIfLoggedIn();
 
 signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const user = await signUpUser(signUpEmail.value, signUpPassword.value);
+    const data = new FormData(signUpForm);
+    const user = await signUpUser(data.get('email'), data.get('password'));
 
     if (user) {
         redirectIfLoggedIn();
@@ -23,7 +19,8 @@ signUpForm.addEventListener('submit', async (e) => {
 
 signInForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const user = await signInUser(signInEmail.value, signInPassword.value);
+    const data = new FormData(signInForm);
+    const user = await signInUser(data.get('email'), data.get('password'));
 
     if (user) {
         redirectIfLoggedIn();
